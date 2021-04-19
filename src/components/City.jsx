c49d61bb05e387905c29e09a663c95d6fd07b87f
Aiza Khan <aizakhan03@gmail.com>
2021-04-19 10:58:00 -1000

    Updating existing code to be less expensive and handling error

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 
 export const City = (Zipcode) => {
-    //essentiall I want to make the page look clean so when nothing is passed in we only see the input bar.
-    
-    if (Zipcode[0] === undefined) {
+
+    //essentially I want to make the page look clean so when nothing is passed in we only see the input bar.
+    if (Zipcode.value == '') {
         return <div></div>
     }
-    else{
+    //if something is passed in with valid value show the Zipcodes.
+    else {
         return (
             <div className="info-card">
                 <div className="card-header">
-                    <strong> Zipcode </strong>
+                    <strong> Zipcodes for this City: </strong>
                 </div>
                 <div className="card-body">
-                    <ul className="zipcode-list">
-                        <li>ZipCode: {Zipcode[0]} </li>
-                    </ul>
+                </div>
+
+                <div className="lol">
+                    <div>
+                        <ul>
+                            {Zipcode.value.map(zipcode => {
+                                return <ul> {zipcode}</ul>
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </div>
-            
         )
     }
-    
-    //if something is passed in with valid value show the Zipcode Bar.
-    
 }
